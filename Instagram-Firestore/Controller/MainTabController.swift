4a9cb2e773de7bc264d0a1fb2440aad7cf677a78
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-25 19:14:32 -0300

    refactoring project and creating MainTabController

---
+++ Instagram-Firestore/Controller/MainTabController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabController.swift
+//  Instagram-Firestore
+//
+//  Created by Ezequiel Parada Beltran on 25/11/2020.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
